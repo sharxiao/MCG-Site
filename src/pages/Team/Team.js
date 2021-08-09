4363ce7cc84fe9af1d39248d29c4bc7ed263a109
HarryZ7G <56236602+HarryZ7G@users.noreply.github.com>
2021-08-09 10:51:02 -0400

    Added member components

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './Team.css';
 
+// Components
+import Background from '../../components/Background/Background.js';
+import TopBar from '../../components/TopBar/TopBar.js';
+import Member from '../../components/Member/Member.js';
+
 function Team(props) {
   return (
     <div className="root">
-      <div>
-        <h1 style={{zIndex: 5}}>
-          Our Team
-        </h1>
+      <div className="team-background"/>
+      <TopBar/>
+      <div className="member-cluster">
+        <Member name="John Deer" title="Mr. President"/>
+        <Member name="John Deer" title="Mr. President"/>
+        <Member name="John Deer" title="Mr. President"/>
+        <Member name="John Deer" title="Mr. President"/>
       </div>
     </div>
   )
